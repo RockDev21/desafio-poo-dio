14be8fe698bbbda07e845372343cfb08f1f751d4
RockDev21 <danny.scholze@unemat.br>
2026-01-12 14:12:28 -0400

    Atulizando mentoria e conteudo

--- desafio-poo-dio/src/br/com/dio/desafio/dominio/Conteudo.java
+++ desafio-poo-dio/src/br/com/dio/desafio/dominio/Conteudo.java
@@ -1,8 +1,27 @@
 package br.com.dio.desafio.dominio;
 
-public class Conteudo {
+public abstract class Conteudo {
 
    protected static final double XP_PADRAO = 10;
 
+   private String tiutlo;
+   private String descricao;
 
+   public abstract double calcularXp();
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getTiutlo() {
+        return tiutlo;
+    }
+
+    public void setTiutlo(String tiutlo) {
+        this.tiutlo = tiutlo;
+    }
 }
