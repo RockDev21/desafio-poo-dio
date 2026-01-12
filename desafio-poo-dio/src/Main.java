e6863b9f1a2ad736412976d259a10d6aad1616b9
RockDev21 <danny.scholze@unemat.br>
2026-01-12 13:48:06 -0400

    implementando mentoria na main e curso

--- desafio-poo-dio/src/Main.java
+++ desafio-poo-dio/src/Main.java
@@ -1,4 +1,7 @@
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args){
@@ -12,8 +15,14 @@ public class Main {
         curso2.setDescricao("descrição curso js");
         curso2.setCargahoraria(8);
 
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
         System.out.println(curso1);
         System.out.println(curso2);
+        System.out.println(mentoria);
 
 
 
