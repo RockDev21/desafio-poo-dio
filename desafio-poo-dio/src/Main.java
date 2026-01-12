55ca25feecbf2817a036446ae1b5c0b537dbde09
RockDev21 <danny.scholze@unemat.br>
2026-01-12 15:17:23 -0400

    Arruamndo a Main

--- desafio-poo-dio/src/Main.java
+++ desafio-poo-dio/src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -18,8 +16,7 @@ public class Main {
         curso2.setDescricao("descrição curso js");
         curso2.setCargahoraria(4);
 
-
-     /**   Conteudo conteudo = new Curso();
+        /**   Conteudo conteudo = new Curso();
         Conteudo conteudo1 = new Mentoria();
 
         List<String> palavras = new ArrayList<>();
@@ -31,9 +28,36 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /**System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);**/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devDanny = new Dev();
+        devDanny.setNome("Danny");
+        devDanny.inscreverbootcamp(bootcamp);
+        System.out.println("Conteúdos Incscirtos Danny" + devDanny.getConteudosInscritos());
+        devDanny.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Incscirtos Danny" + devDanny.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Danny" + devDanny.getConteudosConcluidos());
+
+
+        System.out.println("-------");
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverbootcamp(bootcamp);
+        System.out.println("Conteúdos Incscirtos Camila" + devCamila.getConteudosInscritos());
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Incscirtos Camila" + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Camila" + devCamila.getConteudosConcluidos());
 
 
 
