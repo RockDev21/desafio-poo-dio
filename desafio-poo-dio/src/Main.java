8c0cf6afb000f4bc95f826a83778bfbeecf9e41f
RockDev21 <danny.scholze@unemat.br>
2026-01-12 13:41:30 -0400

    Testando o código, e funcionando com sucesso

--- desafio-poo-dio/src/Main.java
+++ desafio-poo-dio/src/Main.java
@@ -1,5 +1,14 @@
+import br.com.dio.desafio.dominio.Curso;
+
 public class Main {
     public static void main(String[] args){
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargahoraria(8);
+
+        System.out.println(curso1);
+
 
     }
 }
