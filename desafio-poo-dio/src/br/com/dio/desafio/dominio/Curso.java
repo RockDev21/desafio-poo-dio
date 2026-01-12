3f06172b7ae47577d545f2055a8e4e04aade889e
RockDev21 <danny.scholze@unemat.br>
2026-01-12 13:36:08 -0400

    implementando o getters e Satters

--- desafio-poo-dio/src/br/com/dio/desafio/dominio/Curso.java
+++ desafio-poo-dio/src/br/com/dio/desafio/dominio/Curso.java
@@ -5,4 +5,27 @@ public class Curso {
     private String descricao;
     private int cargahoraria;
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargahoraria() {
+        return cargahoraria;
+    }
+
+    public void setCargahoraria(int cargahoraria) {
+        this.cargahoraria = cargahoraria;
+    }
 }
